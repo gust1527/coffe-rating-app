a68201d6b7852bce68e100b3af9d89225ad98c43
Gustav Emil Holm Simonsen <au724577@au.dk>
2024-01-27 18:19:04 +0100

    Started from scratch with navigation-bar example

--- coffe_rating_app_impl/lib/main.dart
+++ coffe_rating_app_impl/lib/main.dart
@@ -1,65 +1,76 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const BottomNavigationBarExampleApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class BottomNavigationBarExampleApp extends StatelessWidget {
+  const BottomNavigationBarExampleApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomePage(),
+    return const MaterialApp(
+      home: CoffeListApp(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+class CoffeListApp extends StatefulWidget {
+  const CoffeListApp({super.key});
 
   @override
-  _HomePageState createState() => _HomePageState();
+  State<CoffeListApp> createState() =>
+      _CoffeListAppState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _CoffeListAppState
+    extends State<CoffeListApp> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
+        title: const Text('BottomNavigationBar Sample'),
       ),
-      body: const Center(child: TextField()),
-      bottomNavigationBar: BottomNavigationbar(),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.coffee_maker),
+            label: 'Currently Brewing',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.coffee_rounded),
+            label: 'List of coffe',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
     );
-  }
-}
-
-class BottomNavigationbar extends StatelessWidget {
-  int CurrentIndex = 0;
-
-  const BottomNavigationbar({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return BottomNavigationBar(items: [
-      BottomNavigationBarItem(
-          icon: Icon(Icons.coffee_maker),
-          label: 'See current coffe-setup'),
-      BottomNavigationBarItem(
-          icon: Icon(Icons.list_alt_outlined),
-          label: 'Coffe history'),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.settings),
-        label: 'Settings',
-      ),
-    ]);
   }
 }
