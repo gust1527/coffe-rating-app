8316b179a4c3481c387a18c18c1df80aa9be226a
Gustav Emil Holm Simonsen <au724577@au.dk>
2024-02-04 23:01:21 +0100

    Updated bean class with beanMaker string descriptor

--- coffe_rating_app_impl/lib/utility/CoffeBeanType.dart
+++ coffe_rating_app_impl/lib/utility/CoffeBeanType.dart
@@ -1,23 +1,27 @@
 class CoffeBeanType {
   final String id;
+  final String beanMaker;
   final String beanType;
   final List<int> beanRating; // Change the type to List<int>
   final bool isInMachine;
 
   CoffeBeanType({
     required this.id,
+    required this.beanMaker,
     required this.beanType,
     required this.beanRating,
     required this.isInMachine,
   });
 
   CoffeBeanType.fromJson(Map<String, dynamic>? json, this.id)
-    : beanType = json?['coffe_bean_type'] as String? ?? '',
+    : beanMaker = json['coffe_bean_maker'] as String? ?? '',
+      beanType = json?['coffe_bean_type'] as String? ?? '',
       beanRating = (json?['bean_rating'] as List<dynamic>?)?.map((rating) => int.parse(rating.toString())).toList() ?? [],
       isInMachine = json?['is_in_machine'] as bool? ?? false;
 
   Map<String, Object?> toJson() {
     return {
+      'coffe_bean_maker' : beanMaker,
       'coffe_bean_type': beanType,
       'bean_rating': beanRating,
     };
